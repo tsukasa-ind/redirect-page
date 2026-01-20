3ba45456784a7aebd9ab683cd8eb9ede41d22e39
TomoHorinouchi <nikotarou517@gmail.com>
2026-01-21 07:46:10 +0900

    ループ防止条件を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-const keyword = "redirecturl" //これのValueをリンクとする
-const qryParam = new URLSearchParams(location.search)
-const url = qryParam.get(keyword)??undefined;
-return (
-  <>
-    <a href={url} target="_blank" rel="noopener noreferrer"> {url}</a>
-  </>)
+  const keyword = "redirecturl"; //これのValueをリンクとする
+  const qryParam = new URLSearchParams(location.search);
+  const url = qryParam.get(keyword);
+  return (
+    <>
+      {url && (
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          {url}
+        </a>
+      )}
+      {!url && <p style={{ color: "#eeeeee" }}>{"パラメータが存在しません"}</p>}
+    </>
+  );
 }
 
-export default App
+export default App;
